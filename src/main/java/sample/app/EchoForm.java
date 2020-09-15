959a59cf89ff92bd8b088bfafa96f93e0a9bfceb
SWA <wadasadamu@gmail.com>
2020-09-16 01:12:19 +0900

    Hello Thymeleaf

---
+++ src/main/java/sample/app/EchoForm.java
@@ -0,0 +1,26 @@
+package sample.app;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class EchoForm implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    @Size(max = 10)
+    @Pattern(regexp=".+@.+")
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
---
+++ src/main/java/sample/app/WelcomeController.java
@@ -0,0 +1,14 @@
+package sample.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
